43763970227d0500a49855508eb212c103b0a855
warathepj <80021220+warathepj@users.noreply.github.com>
2023-03-17 09:48:25 +0700

    before make moving placeholder

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,282 +1,298 @@
 <template>
-    <div class="min-h-screen p-4 text-center bg-teal-300">
-      
-      <div v-if="round < 5">
-        <Suspense>
-          <template #default>
-
-            <h2 class="text-[60px]">{{ emoji }}</h2>
-          </template>
-          <template #fallback>Loading....</template>
-        </Suspense>
-        <div class="h-56 text-2xl">
-
-          <h3 v-show="showHint">{{ hint1 }}</h3>
-            <h3 v-show="showHint2">{{ hint2 }}</h3>
-            <h3 v-show="showGiveUp">{{ giveUp }}</h3>
-        </div>
-        <div class="h-72 p-3 rounded-t-3xl flex flex-col items-center bg-teal-600 fixed bottom-0 left-0 w-full">
-          <input
-            type="text"
-            v-el:focusInput
-            v-model="answer"
-            v-bind:disabled="inputDisabled"
-            class="focus:outline-none input-field"
-          />
-  <div class="flex flex-col">
-    <button @click="checkAnswer" class="w-48 p-2 rounded-xl bg-teal-300 text-2xl text-white">Submit</button>
-        <button @click="displayHint1">Hint 1</button>
-        <button @click="displayHint2">Hint 2</button>
-        <button @click="giveUpFn">Give Up</button>
-        <button @click="giveUpFn">How to play</button>
-        <button v-on:click="generateUniqueEmojis" v-show="showButton">
+  <div class="min-h-screen p-4 text-center bg-teal-300">
+    <div v-if="round < 5">
+      <Suspense>
+        <template #default>
+          <h2 class="text-[60px]">{{ emoji }}</h2>
+        </template>
+        <template #fallback>Loading....</template>
+      </Suspense>
+      <div class="h-56 text-2xl">
+        <h3 v-show="showHint" class="text-yellow-300">{{ hint1 }}</h3>
+        <h3 v-show="showHint2" class="text-yellow-500">{{ hint2 }}</h3>
+        <h3 v-show="showGiveUp" class="text-red-500">{{ giveUp }}</h3>
+      </div>
+      <div
+        class="h-72 p-3 rounded-t-3xl flex flex-col items-center bg-teal-600 fixed bottom-0 left-0 w-full"
+      >
+        <input
+          type="text"
+          v-el:focusInput
+          v-model="answer"
+          v-bind:disabled="inputDisabled"
+          class="focus:outline-none input-field"
+        />
+        <div class="flex flex-col">
+          <button
+            @click="checkAnswer"
+            class="w-48 p-2 rounded-xl bg-teal-300 hover:bg-teal-500 text-2xl text-white"
+          >
+            Submit
+          </button>
+          <button 
+            @click="displayHint1"
+            class="w-48 p-2 rounded-xl bg-yellow-300 hover:bg-yellow-500 text-2xl text-white"
+          >
+            Hint 1
+          </button>
+          <button 
+            @click="displayHint2"
+            class="w-48 p-2 rounded-xl bg-yellow-300 hover:bg-yellow-500 text-2xl text-white"
+          >
+            Hint 2
+          </button>
+          <button 
+            @click="giveUpFn"
+            class="w-48 p-2 rounded-xl bg-red-300 hover:bg-red-500 text-2xl text-white"
+          >
+            Give Up
+          </button>
+          <RouterLink to="/">
+            <button 
+              class="w-48 p-2 rounded-xl bg-teal-300 hover:bg-teal-500 text-2xl text-white"
+            >
+              How to play
+            </button>
+          </RouterLink>
+          <button v-on:click="generateUniqueEmojis" v-show="showButton">
             Restart Game
           </button>
-  </div>
-          
-          
-  
-        
-      </div>
-  
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-//   import emojis from "../db.json";
-  // import emojis from "https://warathepj.github.io/vuejs-emoji-convert.json";
-  import { RouterLink, RouterView } from "vue-router";
-  
-  export default {
-    data() {
-      return {
-        // emojis: emojis.emojis,
-        emojis: [],
-        emoji: "",
-        hint1: "",
-        hint2: "",
-        giveUp: "",
-        answer: "",
-        score: 0,
-        round: 0,
-        randomIndex: null,
-        roundEmojis: [],
-        inputDisabled: true,
-        showButton: false,
-        showHint: false,
-        showHint2: false,
-        showGiveUp: false,
-        // showInput: false
-      };
-    },
-    methods: {
-      correctAlert() {
-        let timerInterval;
-        this.$swal({
-          title: "you're correct!",
-          html: "I will close in <b></b> milliseconds.",
-          timer: 1000,
-          timerProgressBar: true,
-          didOpen: () => {
-            this.$swal.showLoading();
-            const b = this.$swal.getHtmlContainer().querySelector("b");
-            timerInterval = setInterval(() => {
-              b.textContent = this.$swal.getTimerLeft();
-            }, 100);
-          },
-          willClose: () => {
-            clearInterval(timerInterval);
-          },
-        }).then((result) => {
-          /* Read more about handling dismissals below */
-          if (result.dismiss === this.$swal.DismissReason.timer) {
-            console.log("I was closed by the timer");
-          }
-        });
-      },
-      wrongAlert() {
-        let timerInterval;
-        this.$swal({
-          title: "you're wrong!",
-          html: "I will close in <b></b> milliseconds.",
-          timer: 1000,
-          timerProgressBar: true,
-          didOpen: () => {
-            this.$swal.showLoading();
-            const b = this.$swal.getHtmlContainer().querySelector("b");
-            timerInterval = setInterval(() => {
-              b.textContent = this.$swal.getTimerLeft();
-            }, 100);
-          },
-          willClose: () => {
-            clearInterval(timerInterval);
-          },
-        }).then((result) => {
-          /* Read more about handling dismissals below */
-          if (result.dismiss === this.$swal.DismissReason.timer) {
-            console.log("I was closed by the timer");
-          }
-        });
-      },
-      scoreAlert() {
-        let timerInterval;
-        this.$swal({
-          title: `You scored ${this.score} out of 5`,
-          html: "I will close in <b></b> milliseconds.",
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: () => {
-            this.$swal.showLoading();
-            const b = this.$swal.getHtmlContainer().querySelector("b");
-            timerInterval = setInterval(() => {
-              b.textContent = this.$swal.getTimerLeft();
-            }, 100);
-          },
-          willClose: () => {
-            clearInterval(timerInterval);
-          },
-        }).then((result) => {
-          /* Read more about handling dismissals below */
-          if (result.dismiss === this.$swal.DismissReason.timer) {
-            console.log("I was closed by the timer");
-          }
-        });
-      },
-      
-  
-  
-      async generateUniqueEmojis() {
-        let uniqueIndex = false;
-  
-        // Make HTTP request to fetch the data
-        const response = await fetch(
-          "https://warathepj.github.io/vuejs-emoji-convert.json"
-        );
-        const data = await response.json();
+  </div>
+</template>
 
-        this.emojis = data.emojis;
-  
-        while (!uniqueIndex) {
-          let randomIndex = Math.floor(Math.random() * data.emojis.length);
-  
-          if (!this.roundEmojis.includes(randomIndex)) {
-            uniqueIndex = true;
-            this.emoji = data.emojis[randomIndex].emo;
-            this.randomIndex = randomIndex;
-            this.roundEmojis.push(randomIndex);
-          }
+<script>
+//   import emojis from "../db.json";
+// import emojis from "https://warathepj.github.io/vuejs-emoji-convert.json";
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  data() {
+    return {
+      // emojis: emojis.emojis,
+      emojis: [],
+      emoji: "",
+      hint1: "",
+      hint2: "",
+      giveUp: "",
+      answer: "",
+      score: 0,
+      round: 0,
+      randomIndex: null,
+      roundEmojis: [],
+      inputDisabled: true,
+      showButton: false,
+      showHint: false,
+      showHint2: false,
+      showGiveUp: false,
+      // showInput: false
+    };
+  },
+  methods: {
+    correctAlert() {
+      let timerInterval;
+      this.$swal({
+        title: "you're correct!",
+        html: "I will close in <b></b> milliseconds.",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+          this.$swal.showLoading();
+          const b = this.$swal.getHtmlContainer().querySelector("b");
+          timerInterval = setInterval(() => {
+            b.textContent = this.$swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        },
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === this.$swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
         }
-        this.inputDisabled = false; // re-enable input field when done
-        this.showButton = false;
-        this.genEmoji = false;
-        
-  
-        console.log("genEmoji from generateUniqueEmojis() : " + this.genEmoji);
-      },
-  
-      displayHint1() {
-        let currentEmojisIndex = this.emojis.findIndex(
-          (item) => item.emo === this.emoji
-        );
-        this.hint1 = this.emojis[currentEmojisIndex].hint1;
-        this.showHint = true;
-        setTimeout(() => {
-          this.showHint = false;
-        }, 5000);
-        console.log("hint1 : " + this.hint1);
-        this.genEmoji = true;
-        console.log("genEmoji from displayHint1() : " + this.genEmoji);
-      },
-      displayHint2() {
-        let currentEmojisIndex2 = this.emojis.findIndex(
-          (item) => item.emo === this.emoji
-        );
-        this.hint2 = this.emojis[currentEmojisIndex2].hint2;
-        this.showHint2 = true;
-        setTimeout(() => {
-          this.showHint2 = false;
-        }, 5000);
-        console.log("hint2 : " + this.hint2);
-        this.genEmoji = true;
-        console.log("genEmoji from displayHint2() : " + this.genEmoji);
-      },
-  
-      giveUpFn() {
-        let currentEmojisIndex3 = this.emojis.findIndex(
-          (item) => item.emo === this.emoji
-        );
-        this.giveUp = this.emojis[currentEmojisIndex3].giveUp;
-        this.showGiveUp = true;
-        setTimeout(() => {
-          this.showGiveUp = false;
-        }, 5000);
-        this.inputDisabled = true; // disable input field
-  
-        console.log("hint2 : " + this.giveUp);
-        this.genEmoji = true;
-        console.log("genEmoji from giveUpFn() : " + this.genEmoji);
-      },
-  
-      checkAnswer() {
-        if (this.answer.toLowerCase() === this.emojiToWord(this.emoji)) {
-          this.score++;
-          this.correctAlert();
-        } else {
-          this.wrongAlert();
+      });
+    },
+    wrongAlert() {
+      let timerInterval;
+      this.$swal({
+        title: "you're wrong!",
+        html: "I will close in <b></b> milliseconds.",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+          this.$swal.showLoading();
+          const b = this.$swal.getHtmlContainer().querySelector("b");
+          timerInterval = setInterval(() => {
+            b.textContent = this.$swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        },
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === this.$swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
         }
-        this.answer = "";
-        if (this.round < 4) {
-          this.generateNextRound();
-          console.log(this.roundEmojis);
-        } else {
-          console.log(this.roundEmojis);
-          
-          this.scoreAlert();
-  
-          this.roundEmojis = [];
-          this.round = 0;
-          this.score = 0;
-  
-          this.showButton = true;
-  
-          this.inputDisabled = true; // disable input field
+      });
+    },
+    scoreAlert() {
+      let timerInterval;
+      this.$swal({
+        title: `You scored ${this.score} out of 5`,
+        html: "I will close in <b></b> milliseconds.",
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: () => {
+          this.$swal.showLoading();
+          const b = this.$swal.getHtmlContainer().querySelector("b");
+          timerInterval = setInterval(() => {
+            b.textContent = this.$swal.getTimerLeft();
+          }, 100);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        },
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === this.$swal.DismissReason.timer) {
+          console.log("I was closed by the timer");
         }
-        
-      },
-  
-      emojiToWord(emoji) {
-        for (let i = 0; i < this.emojis.length; i++) {
-          if (this.emojis[i].emo === emoji) {
-            return this.emojis[i].giveUp;
-          }
+      });
+    },
+
+    async generateUniqueEmojis() {
+      let uniqueIndex = false;
+
+      // Make HTTP request to fetch the data
+      const response = await fetch(
+        "https://warathepj.github.io/vuejs-emoji-convert.json"
+      );
+      const data = await response.json();
+
+      this.emojis = data.emojis;
+
+      while (!uniqueIndex) {
+        let randomIndex = Math.floor(Math.random() * data.emojis.length);
+
+        if (!this.roundEmojis.includes(randomIndex)) {
+          uniqueIndex = true;
+          this.emoji = data.emojis[randomIndex].emo;
+          this.randomIndex = randomIndex;
+          this.roundEmojis.push(randomIndex);
         }
-        return "";
-      },
-  
-      generateNextRound() {
-        this.round++;
-        if (this.round < 5) {
-          this.generateUniqueEmojis();
-        } else {
-          console.log(this.emojiIndexes); // log the array of emoji indexes to the console
-        }
-      },
-  
-      resetGame() {
-        this.score = 0;
+      }
+      this.inputDisabled = false; // re-enable input field when done
+      this.showButton = false;
+      this.genEmoji = false;
+
+      console.log("genEmoji from generateUniqueEmojis() : " + this.genEmoji);
+    },
+
+    displayHint1() {
+      let currentEmojisIndex = this.emojis.findIndex(
+        (item) => item.emo === this.emoji
+      );
+      this.hint1 = this.emojis[currentEmojisIndex].hint1;
+      this.showHint = true;
+      setTimeout(() => {
+        this.showHint = false;
+      }, 5000);
+      console.log("hint1 : " + this.hint1);
+      this.genEmoji = true;
+      console.log("genEmoji from displayHint1() : " + this.genEmoji);
+    },
+    displayHint2() {
+      let currentEmojisIndex2 = this.emojis.findIndex(
+        (item) => item.emo === this.emoji
+      );
+      this.hint2 = this.emojis[currentEmojisIndex2].hint2;
+      this.showHint2 = true;
+      setTimeout(() => {
+        this.showHint2 = false;
+      }, 5000);
+      console.log("hint2 : " + this.hint2);
+      this.genEmoji = true;
+      console.log("genEmoji from displayHint2() : " + this.genEmoji);
+    },
+
+    giveUpFn() {
+      let currentEmojisIndex3 = this.emojis.findIndex(
+        (item) => item.emo === this.emoji
+      );
+      this.giveUp = this.emojis[currentEmojisIndex3].giveUp;
+      this.showGiveUp = true;
+      setTimeout(() => {
+        this.showGiveUp = false;
+      }, 5000);
+      this.inputDisabled = true; // disable input field
+
+      console.log("hint2 : " + this.giveUp);
+      this.genEmoji = true;
+      console.log("genEmoji from giveUpFn() : " + this.genEmoji);
+    },
+
+    checkAnswer() {
+      if (this.answer.toLowerCase() === this.emojiToWord(this.emoji)) {
+        this.score++;
+        this.correctAlert();
+      } else {
+        this.wrongAlert();
+      }
+      this.answer = "";
+      if (this.round < 4) {
+        this.generateNextRound();
+        console.log(this.roundEmojis);
+      } else {
+        console.log(this.roundEmojis);
+
+        this.scoreAlert();
+
+        this.roundEmojis = [];
         this.round = 0;
+        this.score = 0;
+
+        this.showButton = true;
+
+        this.inputDisabled = true; // disable input field
+      }
+    },
+
+    emojiToWord(emoji) {
+      for (let i = 0; i < this.emojis.length; i++) {
+        if (this.emojis[i].emo === emoji) {
+          return this.emojis[i].giveUp;
+        }
+      }
+      return "";
+    },
+
+    generateNextRound() {
+      this.round++;
+      if (this.round < 5) {
         this.generateUniqueEmojis();
-        this.emojiIndexes = []; // clear the array of emoji indexes when the game is reset
-      },
-  
-      // Define a custom directive that focuses an input element
+      } else {
+        console.log(this.emojiIndexes); // log the array of emoji indexes to the console
+      }
     },
-    mounted() {
+
+    resetGame() {
+      this.score = 0;
+      this.round = 0;
       this.generateUniqueEmojis();
+      this.emojiIndexes = []; // clear the array of emoji indexes when the game is reset
     },
-  };
-  </script>
+
+    // Define a custom directive that focuses an input element
+  },
+  mounted() {
+    this.generateUniqueEmojis();
+  },
+};
+</script>
 
 <style scoped>
 .input-field {
